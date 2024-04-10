e00db52489b5d62baab78be28cc2ba7d28079504
Jessica Xavier <jessicabackend@gmail.com>
2024-04-10 19:30:44 -0300

    Refatorando Documentação

--- src/main/java/br/com/jessica/gestao_cursos/GestaoCursosApplication.java
+++ src/main/java/br/com/jessica/gestao_cursos/GestaoCursosApplication.java
@@ -3,12 +3,6 @@ package br.com.jessica.gestao_cursos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-
 @SpringBootApplication
 public class GestaoCursosApplication {
 
--- src/main/java/br/com/jessica/gestao_cursos/modules/aluno/AlunoEntity.java
+++ src/main/java/br/com/jessica/gestao_cursos/modules/aluno/AlunoEntity.java
@@ -6,11 +6,14 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -21,17 +24,26 @@ public class AlunoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
+    
+    @Schema(example = "Maria Cunha", requiredMode = RequiredMode.REQUIRED, description = "Nome do aluno")
     private String name;
-    @Pattern(regexp = "\\S+", message = "O campo não deve conter espaço")
+
+   @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    @Schema(example = "maria", requiredMode = RequiredMode.REQUIRED, description = "Username do aluno")
     private String username;
 
-    @Email(message = "O campo deve conter um email válido")
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+  @Schema(example = "maria@gmail.com", requiredMode = RequiredMode.REQUIRED, description = "E-mail do aluno")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+  @Schema(example = "maria@1234", minLength = 10, maxLength = 100, requiredMode = RequiredMode.REQUIRED, description = "Senha do aluno")
     private String password;
+
+    @Schema(example = "Desenvolvedor Python", requiredMode = RequiredMode.REQUIRED, description = "Breve descrição do aluno")
     private String description;
+
     private String curriculum;
 
     @CreationTimestamp
